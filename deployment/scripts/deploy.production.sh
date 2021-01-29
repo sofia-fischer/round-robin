2f48dc8c03b48746dc30c62d2bb27e0683a91ef0
Sofia Christ <sofia.christ@tum.de>
2021-01-29 21:58:29 +0100

    fix deploy XI

--- deployment/scripts/deploy.production.sh
+++ deployment/scripts/deploy.production.sh
@@ -2,10 +2,10 @@ echo $HETZNER_SSH_PASSWORD | sudo -kS cp -r dev/round-robin/* /var/www/html/roun
 
 cd /var/www/html/round-robin/
 
+echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R $HETZNER_SSH_USERNAME:$HETZNER_SSH_USERNAME /var/www/html/round-robin
+
 php artisan migrate --force
 
 php artisan config:cache
-
-php artisan route:cache
 
 echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R www-data:www-data /var/www/html/round-robin
