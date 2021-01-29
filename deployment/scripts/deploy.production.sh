54e057170bcedfb9b9a836119f405f9b91160e23
Sofia Christ <sofia.christ@tum.de>
2021-01-29 21:11:15 +0100

    fix deploy VII

--- deployment/scripts/deploy.production.sh
+++ deployment/scripts/deploy.production.sh
@@ -1,4 +1,4 @@
-echo $PASSWORD | sudo -kS cp -r dev/round-robin/* /var/www/html/round-robin/
+echo $HETZNER_SSH_PASSWORD | sudo -kS cp -r dev/round-robin/* /var/www/html/round-robin/
 
 cd /var/www/html/round-robin/
 
