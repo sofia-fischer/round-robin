7b45bcaf0c60c3a064a4ee1de20bee549b1b9988
Sofia Christ <sofia.christ@tum.de>
2021-01-30 17:15:27 +0100

    try fix routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Game;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,11 @@ Route::get('/', \App\View\Pages\WelcomePage::class);
 
 Route::get('/welcome', \App\View\Pages\WelcomePage::class);
 
-Route::get('/group/{group}', \App\View\Pages\GroupRoomPage::class)->middleware(['auth:sanctum']);
 
-Route::get('/group/{group}/game/{game}', \App\View\Pages\GamePage::class)->middleware(['auth:sanctum']);
+Route::get('/game/{game}', function (Game $game) {
+    return redirect()->route('GamePage', ['group' => $game->group->uuid, 'game' => $game->uuid]);
+})->middleware(['auth:sanctum']);
+
+Route::get('/group/{group}/game/{game}', \App\View\Pages\GamePage::class)->middleware(['auth:sanctum'])->name('GamePage');
+
+Route::get('/group/{group}', \App\View\Pages\GroupRoomPage::class)->middleware(['auth:sanctum']);
