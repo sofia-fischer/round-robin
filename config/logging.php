5c03593ae6199c09b605b1414b721730f5f635f0
sofia <sofia@philodev.com>
2022-02-24 19:08:36 +0100

    ✨ install sentry

--- config/logging.php
+++ config/logging.php
@@ -100,16 +100,6 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
-        /*
-         * Custom Channels
-         */
-
-//        'rollbar' => [
-//            'driver'       => 'monolog',
-//            'handler'      => \Rollbar\Laravel\MonologHandler::class,
-//            'access_token' => env('ROLLBAR_TOKEN'),
-//            'level'        => 'debug',
-//        ],
     ],
 
 ];
