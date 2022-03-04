0a67bc0ab7639739aa256f1f4786089f2541eac6
sofia <sofia@philodev.com>
2022-03-04 20:40:20 +0100

    🐛 fix rollbar bug

--- deployment/scripts/deploy.production.sh
+++ deployment/scripts/deploy.production.sh
@@ -1,8 +1,10 @@
 echo $HETZNER_SSH_PASSWORD | sudo -kS cp -r dev/round-robin/* /var/www/html/round-robin/
 
+echo $HETZNER_SSH_PASSWORD | sudo -kS chown www-data:www-data -R /var/www/html/round-robin/
+
 cd /var/www/html/round-robin/
 
-echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R $HETZNER_SSH_USERNAME:$HETZNER_SSH_USERNAME /var/www/html/round-robin
+#echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R $HETZNER_SSH_USERNAME:$HETZNER_SSH_USERNAME /var/www/html/round-robin
 
 php artisan migrate --force
 
@@ -22,4 +24,4 @@ echo $HETZNER_SSH_PASSWORD | sudo -kS cp /var/www/html/round-robin/deployment/co
 #echo $HETZNER_SSH_PASSWORD | sudo -kS supervisorctl update
 #echo $HETZNER_SSH_PASSWORD | sudo -kS supervisorctl start all
 
-echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R www-data:www-data /var/www/html/round-robin
+#echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R www-data:www-data /var/www/html/round-robin
